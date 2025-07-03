17f738edbe69d62bc06e72b4bd2f146414c100ab
khembo <khembo.douglas.juma@gmail.com>
2025-07-03 22:31:50 +0300

    modified

---
+++ alx-movie-app/next.config.mjs
@@ -0,0 +1,17 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+  dest: 'public',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'], // Optional for movie images
+  },
+};
+
+export default withPWA({
+  ...nextConfig,
+});
