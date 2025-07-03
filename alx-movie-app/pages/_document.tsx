3fd1f119161e74b607ade8a6fed4d8d81f036e11
khembo <khembo.douglas.juma@gmail.com>
2025-07-03 23:04:40 +0300

    Link the Manifest in _document.js

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
